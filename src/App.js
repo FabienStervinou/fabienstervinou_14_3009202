4e3f9dfd475502165a8dafed9fb476de6e506349
Fabien Stervinou <stervinoufabien@gmail.com>
2022-10-09 10:44:29 +0200

    Create router and add route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import React from 'react';
 import './App.css';
+import Home from '../src/views/Home/index.jsx';
+import Employees from '../src/views/Employees/index.jsx';
+import Error from '../src/views/Error/index.jsx';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom';
 
-function App() {
+function App () {
   return (
     <div className="App">
-      <header>HRNet 🚀</header>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route exact path="/employees" element={<Employees/>} />
+          <Route exact path="/404" element={<Error/>} /> 
+          <Route path="*" element={<Navigate to="/404" replace={true} />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
