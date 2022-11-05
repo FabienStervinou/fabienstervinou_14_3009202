72bb80c00080457af16460aaa56e51b22af01acf
Fabien Stervinou <stervinoufabien@gmail.com>
2022-11-05 14:25:39 +0100

    <Form> use MUI component textfield and select/option

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,19 +1,25 @@
 import './index.scss';
 import React, { useState } from 'react';
-import Input from './Input/index.jsx';
 import Fieldset from './ Fieldset/index.jsx';
-import Select from './Select/index.jsx';
+import SelectForm from './SelectForm/index.jsx';
 import { data } from '../../utils/data.js';
 import { useDispatch } from 'react-redux';
 import { addEmployee } from '../../features/employees/employeesSlice.js';
+// MUI imports
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
 function Form () {
   const dispatch = useDispatch();
 
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
-  const [dateOfBirth, setDateOfBirth] = useState('');
-  const [startDate, setStartDate] = useState('');
+  const [dateOfBirth, setDateOfBirth] = useState(null);
+  const [startDate, setStartDate] = useState(null);
   const [street, setStreet] = useState('');
   const [city, setCity] = useState('');
   const [stateLocation, setStateLocation] = useState('');
@@ -48,25 +54,81 @@ function Form () {
 
   return (
     <form onSubmit={handleSubmit}>
-      <Input type="text" label="First Name" id="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value) } />
-      <Input type="text" label="Last Name" id="lastName" value={lastName} onChange={(e) => setLastName(e.target.value) } />
-      <Input type="date" label="Date of Birth" id="dateOfBirth" value={dateOfBirth} onChange={(e) => setDateOfBirth(e.target.value) } pattern="\d{4}-\d{2}-\d{2}"/>
-      <Input type="date" label="Start Date" id="startDate" value={startDate} onChange={(e) => setStartDate(e.target.value) } pattern="\d{4}-\d{2}-\d{2}"/>
-      <Fieldset legend="Adresse">
-        <Input type="text" label="Street" id="street" value={street} onChange={(e) => setStreet(e.target.value) } />
-        <Input type="text" label="City" id="city" value={city} onChange={(e) => setCity(e.target.value) } />
-        <Select 
-          options={data.states} 
-          name="State" 
-          label="State" 
-          id="stateLocation" 
-          value={stateLocation}
-          onChange={(e) => setStateLocation(e.target.value)} 
-        />
-        <Input type="number" label="Zip Code" id="zipCode" value={zipCode} onChange={(e) => setZipCode(e.target.value) } />
-      </Fieldset>
-      <Select options={data.department} name="Department" label="Department" id="department" value={department} onChange={(e) => setDepartment(e.target.value) } />
-      <input type="submit" value="Save"/>
+      <Box>
+        <Stack spacing={2}>
+          <TextField
+            id="firstName"
+            label="First Name"
+            value={firstName}
+            onChange={(firstName) => {setFirstName(firstName.target.value);}}
+          />
+          <TextField
+            id="lastName"
+            label="Last Name"
+            value={lastName}
+            onChange={(newValue) => {setLastName(newValue.target.value);}}
+          />
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DesktopDatePicker
+              error={true}
+              label="Date of Birth"
+              inputFormat="DD/MM/YYYY"
+              mask='__/__/____'
+              value={dateOfBirth}
+              onChange={(newValue) => {
+                setDateOfBirth(newValue.format('DD/MM/YYYY'));
+              }}
+              renderInput={(params) => <TextField {...params} />}
+            />
+            <DesktopDatePicker
+              label="Start Date"
+              inputFormat="DD/MM/YYYY"
+              mask='__/__/____'
+              value={startDate}
+              onChange={(newValue) => {
+                setStartDate(newValue.format('DD/MM/YYYY'));
+              }}
+              renderInput={(params) => <TextField fullWidth {...params} />}
+            />
+          </LocalizationProvider>
+        </Stack>
+        <Fieldset legend="Adresse">
+          <TextField
+            sx= {{ m:1 }}
+            id="street"
+            label="Street"
+            value={street}
+            onChange={(newValue) => {setStreet(newValue.target.value);}}
+          />
+          <TextField
+            sx= {{ m:1 }}
+            id="city"
+            label="City"
+            value={city}
+            onChange={(newValue) => {setCity(newValue.target.value);}}
+          />
+          <SelectForm 
+            options={data.states} 
+            name="State" 
+            label="State" 
+            id="stateLocation" 
+            value={stateLocation}
+            onChange={(newValue) => setStateLocation(newValue.target.value)} 
+          />
+          <TextField
+            id="zipCode"
+            label="Zip Code"
+            type="number"
+            value={zipCode}
+            onChange={(newValue) => setZipCode(newValue.target.value)} 
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+        </Fieldset>
+        <SelectForm options={data.department} name="Department" label="Department" id="department" value={department} onChange={(e) => setDepartment(e.target.value) } />
+        <input type="submit" value="Save"/>
+      </Box>
     </form>
   );
 }
